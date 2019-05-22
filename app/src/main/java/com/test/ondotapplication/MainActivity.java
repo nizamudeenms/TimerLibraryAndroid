5b110fb0473622d631dcd6a90be92071ad41dbec
Nizam <nizamudeenms@gmail.com>
2019-05-22 13:17:54 +0530

    Initial Commit

---
+++ app/src/main/java/com/test/ondotapplication/MainActivity.java
@@ -0,0 +1,26 @@
+package com.test.ondotapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        textView = findViewById(R.id.text_view);
+
+
+
+
+    }
+
+
+
+}
---
+++ timerlibrary/src/main/java/com/nizam/timerlibrary/TimerLibraryClass.java
@@ -0,0 +1,22 @@
+package com.nizam.timerlibrary;
+
+import android.os.CountDownTimer;
+import android.widget.TextView;
+
+public class TimerLibraryClass {
+
+
+
+    public static void timerMethod(final TextView textView, String secondsToShow) {
+        new CountDownTimer(10000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                textView.setText("seconds remaining: " + millisUntilFinished / 1000);
+            }
+
+            public void onFinish() {
+                textView.setText("done!");
+            }
+        }.start();
+    }
+}
